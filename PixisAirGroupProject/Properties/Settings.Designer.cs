0225c39ee8ea3c7a14222795860ef57eb6d2b0bc
1212896 <nelson.g1.0508@gmail.com>
2023-11-08 14:05:09 -0600

    Updated

--- PixisAirGroupProject/Properties/Settings.Designer.cs
+++ PixisAirGroupProject/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace PixisAirGroupProject.Properties {
                 return ((string)(this["PixisAirConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=IBMDA400.DataSource.1;Data Source=DEATHSTAR.GTC.EDU;User ID=itpa609;Init" +
+            "ial Catalog=S101FF5C")]
+        public string ConnectionString {
+            get {
+                return ((string)(this["ConnectionString"]));
+            }
+        }
     }
 }
